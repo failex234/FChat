ccdcd1828058668b8b078588017e25af67527f46
Felix Naumann <felix@emilnaumann.de>
2018-04-11 08:29:02 -0700

    moved GUI starter to seperate class to prevent loading the javafx classes when using the server

--- src/main/java/de/failex/fchat/FChat.java
+++ src/main/java/de/failex/fchat/FChat.java
@@ -1,34 +1,14 @@
 package de.failex.fchat;
 
-import de.failex.fchat.gui.MainGUI;
-import de.failex.fchat.gui.MainGUIController;
+import de.failex.fchat.gui.StartGUI;
 import de.failex.fchat.server.Server;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class FChat extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        //Setup GUI and start controller
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResourceAsStream("/maingui.fxml"));
-        primaryStage.setTitle("FChat");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-        new MainGUI((MainGUIController) loader.getController(), primaryStage);
-        primaryStage.show();
-    }
-
+public class FChat {
 
     public static void main(String[] args) {
         //Start the server when the program starts with an argument
         if (args.length == 0) {
-            launch(args);
-            System.exit(0);
+            StartGUI.runGUI(args);
         } else {
             try {
                 int port = Integer.parseInt(args[0]);
---
+++ src/main/java/de/failex/fchat/gui/StartGUI.java
@@ -0,0 +1,26 @@
+package de.failex.fchat.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartGUI extends Application {
+
+    public static void runGUI(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Setup GUI and start controller
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResourceAsStream("/maingui.fxml"));
+        primaryStage.setTitle("FChat");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        new MainGUI(loader.getController(), primaryStage);
+        primaryStage.show();
+    }
+}
